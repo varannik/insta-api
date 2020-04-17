bef4f2978457431ef2ccbd45b8f3f344151bc0bd
mohammadreza mohammadi <mohammadrezamohammadi@VARANIK.local>
2020-04-17 13:01:55 +0430

    Cleaning code for linter check

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-from django.contrib.auth import  get_user_model
+from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase): 
+
+class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         "Test creating a new user with an email is successful"
@@ -10,35 +11,31 @@ class ModelTests(TestCase):
         password = '52041'
 
         user = get_user_model().objects.create_user(
-            email = email,
-            password= password
+            email=email,
+            password=password
         )
-        self.assertEqual(user.email , email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    
     def test_new_user_email_normalize(self):
         """ Test the email for a new user is normalize """
 
         email = "varanik@GMAIL.COM"
-        user = get_user_model().objects.create_user(email , '52041')
-        
-        self.assertEqual(user.email , email.lower())
+        user = get_user_model().objects.create_user(email, '52041')
+        self.assertEqual(user.email, email.lower())
 
-    
     def test_new_user_invalid_email(self):
         """Test Creating user with no email raises error"""
 
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None,'52041')
-
+            get_user_model().objects.create_user(None, '52041')
 
     def test_create_new_superuser(self):
-        """Test Creating a new super user """
+        """Test Creating a new super user"""
 
         user = get_user_model().objects.create_superuser(
-            email = 'varanik@gmail.com',
-            password = "52041"
+            email='varanik@gmail.com',
+            password="52041"
         )
 
         self.assertTrue(user.is_superuser)
