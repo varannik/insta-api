89074f0c4a221ba721babb8cf53aef03991f4ee0
Varanik <varannik5@gmail.com>
2020-07-20 16:40:11 +0430

    change to production

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+l@%ia8#ozqaccmn^4++e+ufap&*-e&47#19m4@z%3$9*@1pdu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
